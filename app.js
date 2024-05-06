16aa62c5b321423a7e6b054c60e22b67a7a2f958
Ibro Rebronja <27772860+IbroRebronja@users.noreply.github.com>
2024-05-06 15:18:30 +0200

    added some interactibility for 768px screen

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-var siteWidth = 1280;
-var scale = screen.width /siteWidth;
+//var siteWidth = 1280;
+//var scale = screen.width /siteWidth;
+//
+//document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
+//
 
-document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
+
+
+
