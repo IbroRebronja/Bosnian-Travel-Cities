e93e95d1530878583bcc504014ac9bbca97fdd9f
Ibro Rebronja <27772860+IbroRebronja@users.noreply.github.com>
2024-05-14 12:13:25 +0200

    made javascript folder and put app.js into it, added myscripts.js for contactus.html to show password on click

---
+++ javascript/myscripts.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var x = document.getElementById("userpassword");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+  
