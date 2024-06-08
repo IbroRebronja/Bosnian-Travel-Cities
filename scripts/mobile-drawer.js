61a8360bd44df50e23a8b8ed612c3f3c9f55489f
Ibro Rebronja <27772860+IbroRebronja@users.noreply.github.com>
2024-06-08 13:35:08 +0200

    added node modules, express, nodemon, some folders for future project use, ejs, modified .gitignore, renamed app.js into mobile-drawer.js

---
+++ scripts/mobile-drawer.js
@@ -0,0 +1,23 @@
+//var siteWidth = 1280;
+//var scale = screen.width /siteWidth;
+//
+//document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
+//
+
+//ignore this for now, was an attempt to get the 'viewing' on all devices xdd
+
+//dropdown menu
+
+document.addEventListener("DOMContentLoaded", function () {
+    const dropdown = document.querySelector(".dropdown");
+    const dropdownContent = document.querySelector(".dropdown-content");
+  
+    dropdown.addEventListener("mouseover", function () {
+      dropdownContent.style.display = "block";
+    });
+  
+    dropdown.addEventListener("mouseout", function () {
+      dropdownContent.style.display = "none";
+    });
+  });
+  
